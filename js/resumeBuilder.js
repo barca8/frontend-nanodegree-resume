443e103a3004428fcee44843ff3cbc5a3a425bad
barca8 <barca8best@gmail.com>
2017-07-16 12:43:58 +0800

    Add name and role to the header Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
-$("#main").append("barca8");
+var name = "Lion Messi";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
