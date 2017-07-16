80a0a940c14e8209fc2e103016668de2a9e95538
barca8 <barca8best@gmail.com>
2017-07-16 20:51:53 +0800

    Add work and project objects to resumeBuilder.js Finish my bio and education objects to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,85 @@
-var name = "Lion Messi";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var work = {
+  "jobs": [
+    {
+      "employer": "Alibaba",
+      "title": "Developer",
+      "location": "Hangzhou",
+      "dates": "2008 - 2010",
+      "descripion": "my first job"
+    },
+    {
+      "employer": "Baidu",
+      "title": "Developer",
+      "location": "Beijing",
+      "dates": "2010 - 2011",
+      "descripion": "my second job"
+    },
+    {
+      "employer": "Huawei",
+      "title": "Engineer",
+      "location": "Ningbo",
+      "dates": "2011 - Now",
+      "descripion": "it's my job now."
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "Sample Project 1",
+      "dates":  "2015",
+      "description": "this project is a sample project for test.",
+      "images": [
+        "https://",
+        "https://"
+      ]
+    }
+  ]
+};
+
+var bio = {
+  "name": "Lion Messi",
+  "role": "Web Developer",
+  "welcomeMessage": "welcome to my resume.",
+  "bioPic": "images/fry.jpg",
+  "contacts": {
+    "mobile": "0574-87533703",
+    "email": "messi@gmail.com",
+    "github": "messi",
+    "twitter": "@messi",
+    "location": "Ningbo"
+  },
+  "skills": [
+    "football", "programming", "sleeping", "drawing"
+  ]
+};
+
+var education = {
+  "schools": [
+  {
+      "name": "Zhejiang University",
+      "city": "Hangzhou",
+      "degree": "Masters",
+      "majors": ["CS"],
+      "dates": "2011",
+      "url": "http://example.com"
+      },
+    {
+      "name": "Ningbo University",
+      "city": "Ningbo",
+      "degree": "BA",
+      "majors": ["CS"],
+      "dates": "2008",
+      "url": "http://example.com"
+      }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JaveScript Crash Course",
+      "school": "Udacity",
+      "dates": "2017",
+      "url": "http://www.udacity.com/course/ud804"
+    }
+  ]
+};
